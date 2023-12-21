4f42a5634a4c2957e1262483e20bfb44e572464e
Anthony <geant@seas.upenn.edu>
2023-12-21 16:34:32 -0500

    implemented post process outlines

--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -10,3 +10,56 @@ void GetNormal_float(float2 uv, out float3 Normal)
 {
     Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
 }
+
+static float2 sobelSamplePoints[9] =
+{
+    float2(-1, 1), float2(0, 1), float2(1, 1),
+    float2(-1, 0), float2(0, 0), float2(1, 1),
+    float2(-1, -1), float2(0, -1), float2(1, -1),
+};
+
+// x weights
+static float sobelXMatrix[9] =
+{
+    1, 0, -1,
+    2, 0, -2,
+    1, 0, -1
+};
+
+// y weights
+static float sobelYMatrix[9] =
+{
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1
+};
+
+void DepthSobel_float(float2 uv, float Thickness, out float Out) {
+    float2 sobel = 0;
+
+    [unroll] for (int i = 0; i < 9; i++) {
+        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(uv + sobelSamplePoints[i] * Thickness);
+        sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
+    }
+
+    Out = length(sobel);
+}
+
+void ColorSobel_float(float2 UV, float Thickness, out float OUT)
+{
+    float2 sobelR = 0;
+    float2 sobelG = 0;
+    float2 sobelB = 0;
+
+    for (int i = 0; i < 9; i++)
+    {
+		float3 rgb = SAMPLE_TEXTURE2D(_MainTex, sampler_point_clamp, UV + sobelSamplePoints[i] * Thickness).rgb;
+        float2 kernel = float2(sobelXMatrix[i], sobelYMatrix[i]);
+        
+        sobelR += rgb.r * kernel;
+        sobelG += rgb.g * kernel;
+        sobelB += rgb.b * kernel;
+    }
+    
+    OUT = max(length(sobelR), max(length(sobelG), length(sobelB)));
+}
